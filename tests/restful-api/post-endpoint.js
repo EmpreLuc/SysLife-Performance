deee27530f2bbb99d3a9a0c899cf7e03cc59f42c
Lucas yoris <empresa.lucasyoris@gmail.com>
2024-05-10 01:33:29 -0300

    changes

--- tests/restful-api/post-endpoint.js
+++ tests/restful-api/post-endpoint.js
@@ -4,8 +4,6 @@ import loadConfig from '../../setup/loadConfig.js';
 import postApiThresholdsConfig from '../../setup/restful-api/postApiThresholdsConfig.js';
 import { getRandomInt } from './../../utils/random.js';
 import { cargarHeadersDesdeJSON } from '../../utils/headersUtils.js';
-import generateReport from '../../utils/report.js';
-import httpagg from 'k6/x/httpagg';
 
 const testType = __ENV.TEST_TYPE;
 const rps = __ENV.RPS || 0;
@@ -18,16 +16,16 @@ export const options = Object.assign({
     rps: rps,
 }, loadConfig[testType], postApiThresholdsConfig[testType])
 
-export function setup() {
+/* export function setup() {
     console.log(JSON.stringify(options, null, 2));
-}
+} */
 
 const body = JSON.parse(open(jsonBody));
 const headers = cargarHeadersDesdeJSON(jsonHeaders, 0);
 const urls = JSON.parse(open(jsonUrl));
 const url = urls[0].baseUrl + urls[0].postUrl
 
-export default function(){
+export default function () {
     group('POST /objects', function () {
         console.log('BODY: ' + JSON.stringify(body[getRandomInt(body.length)]));
         console.log('HEADERS: ' + JSON.stringify(headers));
